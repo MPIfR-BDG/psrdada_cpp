361d677f1770bbf31c88566773d275da3b4372c1
Ewan Barr <ewan.d.barr@googlemail.com>
2017-08-02 22:17:18 +0200

    moved around comments

--- psrdada_cpp/meerkat/tools/feng_to_bandpass.cuh
+++ psrdada_cpp/meerkat/tools/feng_to_bandpass.cuh
@@ -12,6 +12,34 @@ namespace meerkat {
 namespace tools {
 namespace kernels {
 
+    /**
+     * @brief      Convert data that is in MeerKAT F-engine order into
+     *             a spectrum for each antenna
+     *
+     * @detail     The heaps from the the MeerKAT F-engine are in in FTP
+     *             order with the T = 256 and P = 2. The number of frequency
+     *             channels in a heap is variable but is always a power of two.
+     *             The data itself is 8-bit complex (8-bit real, 8-bit imaginary).
+     *             In this kernel we perform a vectorised char2 load such that
+     *             each thread gets a complete complex voltage.
+     *
+     *             As each block with process all heaps from a given antenna
+     *             for a given frequency channel we use 512 threads per block
+     *             which matches nicely with the inner TP dimensions of the heaps.
+     *
+     *             The heaps themselves are ordered in TAF (A=antenna) order. As
+     *             such the full order of the input can be considered to be
+     *             tAFFTP which simplifies to tAFTP (using small t and big T
+     *             to disambiguate between the two time axes). Each block of
+     *             threads will process TP for all t (for one A and one F).
+     *
+     * @param      in               Input buffer
+     * @param      out              Output buffer
+     * @param[in]  nchans           The number of frequency chans
+     * @param[in]  nants            The number of antennas
+     * @param[in]  ntimestamps      The number of timestamps (this corresponds to the
+     *                              number of heaps in the time axis)
+     */
     __global__
     void feng_heaps_to_bandpass(
         char2* __restrict__ in, float* __restrict__ out,
