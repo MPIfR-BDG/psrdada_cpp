ce45d105ac2a85e0a50ce2454623bb08a0b82b6e
Ewan Barr <ewan.d.barr@googlemail.com>
2017-08-11 19:12:46 +0200

    added test code

--- psrdada_cpp/meerkat/tools/src/feng_to_dada_test.cu
+++ psrdada_cpp/meerkat/tools/src/feng_to_dada_test.cu
@@ -3,27 +3,44 @@
 #include "psrdada_cpp/raw_bytes.hpp"
 #include "psrdada_cpp/multilog.hpp"
 
+#define NCHANS 256
+#define NTIMESTAMPS 800
+
 using namespace psrdada_cpp;
 
 struct FengToDadaTester
 {
     void init(RawBytes&){}
-    bool operator()(RawBytes&){return false;}
+    bool operator()(RawBytes& block)
+    {
+        int* buffer = (int*) block.ptr();
+
+        for (int timestamp=0; timestamp<NTIMESTAMPS*MEERKAT_FENG_NSAMPS_PER_HEAP; ++timestamp)
+        {
+            for (int chan=0; chan<NCHANS; ++chan)
+            {
+                int offset = timestamp * NCHANS + chan;
+                if (buffer[offset] != chan)
+                {
+                    std::cerr << "Failure at timestamp: " << timestamp << "  channel: " << chan << std::endl;
+                }
+            }
+        }
+        return false;
+    }
 };
 
 int main()
 {
-    std::size_t const nchans = 256;
-    std::size_t const ntimestamps = 800;
-    std::size_t const nelements_per_timestamp = nchans * MEERKAT_FENG_NSAMPS_PER_HEAP;
-    std::size_t const nelements = nelements_per_timestamp * ntimestamps;
+    std::size_t const nelements_per_timestamp = NCHANS * MEERKAT_FENG_NSAMPS_PER_HEAP;
+    std::size_t const nelements = nelements_per_timestamp * NTIMESTAMPS;
     std::size_t const used_bytes = nelements * MEERKAT_FENG_NBYTES_PER_SAMPLE * MEERKAT_FENG_NPOL_PER_HEAP;
 
     std::vector<int> input_buffer(nelements);
 
-    for (int timestamp=0; timestamp<ntimestamps; ++timestamp)
+    for (int timestamp=0; timestamp<NTIMESTAMPS; ++timestamp)
     {
-        for (int chan=0; chan<nchans; ++chan)
+        for (int chan=0; chan<NCHANS; ++chan)
         {
             for (int samp=0; samp<MEERKAT_FENG_NSAMPS_PER_HEAP; ++samp)
             {
@@ -36,6 +53,6 @@ int main()
     }
     FengToDadaTester tester;
     RawBytes bytes((char*)input_buffer.data(),used_bytes,used_bytes);
-    meerkat::tools::FengToDada<FengToDadaTester> proc(nchans, tester);
+    meerkat::tools::FengToDada<FengToDadaTester> proc(NCHANS, tester);
     proc(bytes);
 };
