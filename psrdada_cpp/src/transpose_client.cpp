93e3fb0ff29d8a6f31e2f4da64621d37223e81f6
kmrajwade <rkaustubh10@gmail.com>
2018-03-16 16:30:20 +0000

    Added setters for transpose block

---
+++ psrdada_cpp/src/transpose_client.cpp
@@ -0,0 +1,139 @@
+#include "psrdada_cpp/transpose_client.hpp"
+#include "psrdada_cpp/cli_utils.hpp"
+#include "psrdada_cpp/multilog.hpp"
+
+namespace psrdada_cpp {
+
+    TransposeClient::TransposeClient(DadaWriteClient* _writer[],std::string* keys,std::uint32_t numbeams)
+    : _nbeams(numbeams),
+      _nchans(128),
+      _ntime(64),
+      _nsamples(128),
+      _nfreq(32)
+    {
+        int ii;
+        std::string logname="logger";
+        MultiLog log(logname);
+        _writer = new DadaWriteClient*[numbeams];
+        key_t key;
+        for (ii=0 ; ii< numbeams; ii ++)
+        {
+            key = string_to_key(*(keys + ii));
+            *(_writer + ii) = new DadaWriteClient(key, log)  ;
+        }
+
+    }
+
+    TransposeClient::~TransposeClient()
+    {
+    }
+
+    /**
+     * @brief: The method that transposes per beam. User needs to initiate multiple threads corresponding to multiple beams
+     */
+
+
+    RawBytes& TransposeClient::do_transpose(RawBytes& input_data,std::uint32_t beamnum)
+    {
+
+
+        // For now making a separate output memory block in RAM and constructing a RawBytes object pointing to it
+	// Can be done more memory efficiently by using a temporary pointer
+	
+        char* o_data =NULL;
+
+	o_data = new char[_nsamples*_ntime*_nfreq*_nchans];
+        std::uint32_t i,j,k,l,m;
+	std::uint32_t a = 0;
+
+	RawBytes transposed_data(o_data,std::size_t(_nsamples*_ntime*_nfreq*_nchans),std::size_t(0));
+
+        // Actual transpose
+
+	    for (j =0; j < _nsamples; j++)
+            {
+                for (k = 0; k < _ntime ; k++)
+		{
+
+		    for (l = 0; l < _nfreq ; l++)
+		    {
+
+										                         
+		        for (m=0;m < _nchans ; m++)
+		        {
+
+		            transposed_data.ptr()[a] = input_data.ptr()[m + _ntime * _nchans * _nsamples * l + _nchans * (j * _ntime + k) + _nsamples * _nchans * _ntime* _nfreq * beamnum];
+			    a++;
+
+		        }
+					                            																		                                                                             
+		    }
+
+																						
+	        }                                                                                                       
+                                                                                                      
+	    }
+	 
+	 
+	
+        return std::ref(transposed_data);
+         
+    }
+
+   /* @brief: Function that reads in the data from the DADA buffer adn returns a Reference to a RawBytes object */
+
+
+    RawBytes& TransposeClient::read_to_transpose(DadaReadClient& _reader)
+    {
+        // Get the current block of data
+        DadaReadClient::DataStream& data_stream = _reader.data_stream();
+        // Get the RawBytes object pointing to the current block of memory
+        auto& current_block = data_stream.next();
+       // Release the current block of data
+        data_stream.release();
+	return std::ref(current_block);
+    }
+
+
+    /* @brief: Method to write the block of transposed data to a DADA buffer.*/
+
+    void TransposeClient::write_transpose(RawBytes& transposed_data, DadaWriteClient& writer)
+    {
+        DadaWriteClient::DataStream& data_stream = writer.data_stream();
+	auto& current_block = data_stream.next();
+	//Write the transposed data(std::memcpy??)
+	std::memcpy(current_block.ptr(), transposed_data.ptr(),transposed_data.used_bytes());
+        current_block.used_bytes(transposed_data.used_bytes());
+	data_stream.release();
+
+    }
+
+
+    /* All the setters for setting up the transpose client block */
+
+    void TransposeClient::set_nbeams(const int nbeams)
+    {
+         _nbeams = nbeams;
+    }
+
+    void TransposeClient::set_nchans(const int nchans)
+    {
+        _nchans = nchans;
+    }
+
+    void TransposeClient::set_ntime(const int ntime)
+    {
+        _ntime = ntime;
+    }
+
+    void TransposeClient::set_nsamples(const int nsamples)
+    {
+        _nsamples = nsamples;
+    }
+
+    void TransposeClient::set_nfreq(const int nfreq)
+    {
+        _nfreq = nfreq;
+    }
+
+} //namespace psrdada_cpp
---
+++ psrdada_cpp/transpose_client.hpp
@@ -0,0 +1,83 @@
+#ifndef PSRDADA_CPP_TRANSPOSE_CLIENT_HPP
+#define PSRDADA_CPP_TRANSPOSE_CLIENT_HPP
+
+#include "psrdada_cpp/dada_read_client.hpp"
+#include "psrdada_cpp/dada_write_client.hpp"
+#include "psrdada_cpp/raw_bytes.hpp"
+#include "psrdada_cpp/common.hpp"
+
+
+namespace psrdada_cpp {
+
+    /**
+     * @brief      Class that provides means for doing transpose
+     *             by reading data from a DADA ring buffer and 
+     *             write out the transposed data to a DADA ring
+     *             buffer
+     */
+    class TransposeClient 
+    {
+    public:
+
+        TransposeClient(DadaWriteClient* _writer[],std::string* keys, std::uint32_t numbeams);
+        TransposeClient(TransposeClient const&) = delete;
+	~TransposeClient();
+
+        /**
+	 * @brief function to the do the transpose
+	 */
+        RawBytes& do_transpose(RawBytes& _current_block, std::uint32_t beamnum); 
+        
+	/**
+	 * @brief Function to read in the data to be transposed
+	 */
+        RawBytes& read_to_transpose(DadaReadClient& _reader);
+
+        /**
+	 * @brief Function to write transposed data to the dada buffer
+	 */
+        void write_transpose(RawBytes& transposed_data, DadaWriteClient& writer);
+
+
+       /**
+        * @brief Setter for the numbeams
+        */
+
+        void set_nbeams(const int nbeams);   
+     
+       /**
+        * @brief Setter for frequency channels
+        */
+
+        void set_nchans(const int nchans);
+
+       /**
+        * @brief Setter of number of samples
+        */
+
+        void set_nsamples(const int nsamples);
+  
+       /**
+        * @brief Setter for number of time samples
+        */
+
+        void set_ntime(const int ntime);
+
+       /**
+        * @brief Setter for number of frequency blocks
+        */
+
+        void set_nfreq(const int _nfreq);
+
+
+    private:
+        std::uint32_t _nbeams;
+	std::uint32_t _nchans;
+	std::uint32_t _nsamples;
+	std::uint32_t _ntime;
+	std::uint32_t _nfreq;
+
+    };
+} //namespace psrdada_cpp
+
+#endif //PSRDADA_CPP_TRANSPOSE_CLIENT_HPP
