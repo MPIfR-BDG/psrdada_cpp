bf1d8b2af1a4a5a223713916513ca6516e4e0b6c
Kaustubh Rajwade <kaustub@Kaustubhs-MacBook-Pro.local>
2018-05-25 15:52:30 +0100

    Added more code to header converter (work in progress)

--- psrdada_cpp/detail/psrdada_to_sigproc_header.cpp
+++ psrdada_cpp/detail/psrdada_to_sigproc_header.cpp
@@ -1,4 +1,6 @@
 #include "psrdada_cpp/psrdada_to_sigproc_header.hpp"
+#include "psrdada_cpp/psrdadaheader.hpp"
+#include "psrdada_cpp/sigprocheader.cpp"
 #include "psrdada_cpp/cli_utils.hpp"
 #include <thread>
 
@@ -19,6 +21,11 @@ namespace psrdada_cpp {
     void PsrDadaToSigprocHeader<HandlerType>::init(RawBytes& block)
     {
     	/* Do the psrdada to sigproc conversion here */
+	SigProcHeader h;
+	PsrDadaHeader ph;
+	ph.from_bytes(block);
+	//Not sure if overwriting works--KR
+        h.to_bytes(block,ph);
 	_handler.init(block);
     }
 
