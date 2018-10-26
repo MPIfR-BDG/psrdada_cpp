6578d08572c4649259ef340a11a8050b22a4d566
Ewan Barr <ewan.d.barr@googlemail.com>
2018-10-26 16:52:23 +0200

    added googletest

---
+++ cmake/googletest.cmake
@@ -0,0 +1,15 @@
+# == googletest build - must be build with the same compiler flags
+if(NOT GTEST_INCLUDE_DIR)
+    if(APPLE)
+    add_definitions(-DGTEST_USE_OWN_TR1_TUPLE=1)
+    else(APPLE)
+    add_definitions(-DGTEST_USE_OWN_TR1_TUPLE=0)
+    endif(APPLE)
+
+    add_subdirectory("thirdparty/googletest")
+    set(GTEST_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/googletest/googletest/include)
+    set(GMOCK_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/googletest/googlemock/include)
+    set(GTEST_LIBRARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/googletest)
+    message("BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}")
+    set(GTEST_LIBRARIES gtest_main gtest)
+endif()
