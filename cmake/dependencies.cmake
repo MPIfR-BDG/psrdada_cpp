47f7fbe8ddeacdf290b95d5b0bb99b599bcd05b4
Ewan Barr <ewan.d.barr@googlemail.com>
2018-10-26 18:49:59 +0200

    added gtest to dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,6 @@
 include(cuda)
 include(compiler_settings)
+include(cmake/googletest.cmake)
 include(cmake/boost.cmake)
 include(cmake/psrdada.cmake)
 include_directories(SYSTEM ${Boost_INCLUDE_DIR} ${PSRDADA_INCLUDE_DIR})
