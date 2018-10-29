fa625446ef253e2711d5b18b00d79eb9d7901117
Ewan Barr <ewan.d.barr@googlemail.com>
2018-10-29 11:07:15 +0100

    ../../../../cmake/dependencies.cmake

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,6 +4,7 @@ include(cmake/googletest.cmake)
 include(cmake/boost.cmake)
 include(cmake/psrdada.cmake)
 include_directories(SYSTEM ${Boost_INCLUDE_DIR} ${PSRDADA_INCLUDE_DIR})
+include_directories(BEFORE ${GTEST_INCLUDE_DIRS})
 set(DEPENDENCY_LIBRARIES
     ${GTEST_LIBRARIES}
     ${Boost_LIBRARIES}
