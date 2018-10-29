ec9c1ddeca28c098469cf147d39f21988617c497
Ewan Barr <ewan.d.barr@googlemail.com>
2018-10-29 11:05:15 +0100

    added missing gtest dependency

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -5,6 +5,7 @@ include(cmake/boost.cmake)
 include(cmake/psrdada.cmake)
 include_directories(SYSTEM ${Boost_INCLUDE_DIR} ${PSRDADA_INCLUDE_DIR})
 set(DEPENDENCY_LIBRARIES
+    ${GTEST_LIBRARIES}
     ${Boost_LIBRARIES}
     ${PSRDADA_LIBRARIES}
     ${CUDA_CUDART_LIBRARY}
