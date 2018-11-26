554e3ba5f99982a1951a32064aeb031da889e90b
Ewan Barr <ewan.d.barr@googlemail.com>
2018-11-26 16:29:21 +0100

    first pass on pipeline tester

---
+++ psrdada_cpp/meerkat/fbfuse/test/src/PipelineTester.cu
@@ -0,0 +1,110 @@
+#include "psrdada_cpp/meerkat/fbfuse/test/PipelineTester.cuh"
+#include "psrdada_cpp/meerkat/fbfuse/fbfuse_constants.hpp"
+#include "psrdada_cpp/meerkat/fbfuse/PipelineConfig.hpp"
+#include "psrdada_cpp/meerkat/fbfuse/DelayEngineSimulator.cuh"
+#include "psrdada_cpp/dada_db.hpp"
+#include "psrdada_cpp/common.hpp"
+#include "psrdada_cpp/cuda_utils.hpp"
+#include <random>
+#include <cmath>
+#include <complex>
+
+namespace psrdada_cpp {
+namespace meerkat {
+namespace fbfuse {
+namespace test {
+
+PipelineTester::PipelineTester()
+    : ::testing::Test()
+{
+}
+
+PipelineTester::~PipelineTester()
+{
+}
+
+void PipelineTester::SetUp()
+{
+    _config.centre_frequency(1.4e9);
+    _config.bandwidth(56.0e6);
+    _config.calculate_channel_frequencies();
+    _config.delay_buffer_shm("test_delay_buffer_shm");
+    _config.delay_buffer_sem("test_delay_buffer_sem");
+    _config.delay_buffer_mutex("test_delay_buffer_mutex");
+}
+
+void PipelineTester::TearDown()
+{
+}
+
+TEST_F(PipelineTester, simple_run_test)
+{
+
+    DelayEngineSimulator simulator(_config);
+
+    int const ntimestamps_per_block = 64;
+    int const taftp_block_size = (ntimestamps_per_block * _config.total_nantennas()
+        * _config.nchans() * _config.nsamples_per_heap() * _config.npol() * 2);
+    int const taftp_block_bytes = taftp_block_size * sizeof(char2);
+
+    //Create output buffer for coherent beams
+    int const cb_output_nsamps = _config.nsamples_per_heap() * ntimestamps_per_block / _config.cb_tscrunch();
+    int const cb_output_nchans = _config.nchans() / _config.cb_fscrunch();
+    int const cb_block_size = _config.cb_nbeams() * cb_output_nsamps * cb_output_nchans;
+    DadaDb cb_buffer(8, cb_block_size, 4, 4096);
+    cb_buffer.create();
+    _config.cb_dada_key(cb_buffer.key());
+    DadaWriteClient
+
+    //Create output buffer for incoherent beams
+    int const ib_output_nsamps = _config.nsamples_per_heap() * ntimestamps_per_block / _config.ib_tscrunch();
+    int const ib_output_nchans = _config.nchans() / _config.ib_fscrunch();
+    int const ib_block_size = _config.ib_nbeams() * ib_output_nsamps * ib_output_nchans;
+    DadaDb ib_buffer(8, ib_block_size, 4, 4096);
+    ib_buffer.create();
+    _config.ib_dada_key(ib_buffer.key());
+
+    //Setup write clients
+    MultiLog log("PipelineTester")
+    DadaWriteClient cb_write_client(_config.cb_dada_key());
+    DadaWriteClient ib_write_client(_config.ib_dada_key());
+    Pipeline pipeline(_config, cb_write_client, ib_write_client, taftp_block_bytes);
+
+    //Set up null sinks on all buffers
+    NullSink null_sink;
+    DadaInputStream<NullSink> cb_consumer(_config.cb_dada_key(), log, null_sink);
+    DadaInputStream<NullSink> ib_consumer(_config.cb_dada_key(), log, null_sink);
+    std::thread cb_consumer_thread(cb_consumer.start);
+    std::thread ib_consumer_thread(ib_consumer.start);
+
+    //Create and input header buffer
+    std::vector<char> input_header_buffer(4096, 0);
+    RawBytes input_header_rb(input_header_buffer.data(), 4096, 4096);
+    Header header(input_header_rb);
+    header.set<std::size_t>("SAMPLE_CLOCK_START", 0L);
+    header.set<long double>("SAMPLE_CLOCK", 856000000.0);
+    header.set<long double>("SYNC_TIME", 0.0);
+
+    //Create and input data buffer
+    std::vector<int8_t> input_data_buffer(taftp_block_bytes);
+    RawBytes input_data_rb(input_data_buffer.data(), taftp_block_bytes, taftp_block_bytes);
+
+    //Run the init
+    pipeline.init(input_header_rb);
+    //Loop over N data blocks and push them through the system
+    for (int ii = 0; ii < 10; ++ii)
+    {
+        pipeline.process(input_data_rb);
+    }
+    cb_consumer.stop();
+    ib_consumer.stop();
+    pipeline.process(input_data_rb);
+    cb_consumer_thread.join();
+    ib_consumer_thread.join();
+}
+
+} //namespace test
+} //namespace fbfuse
+} //namespace meerkat
+} //namespace psrdada_cpp
+
