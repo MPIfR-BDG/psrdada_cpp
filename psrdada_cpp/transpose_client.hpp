fe0d476bfd9fa1bb8018a20c877e1b5a35c85d12
Ewan Barr <ewan.d.barr@googlemail.com>
2018-12-03 18:15:18 +0100

    changed indentation

--- psrdada_cpp/transpose_client.hpp
+++ psrdada_cpp/transpose_client.hpp
@@ -9,105 +9,96 @@
 
 namespace psrdada_cpp {
 
+/**
+ * @brief      Class that provides means for doing transpose
+ *             by reading data from a DADA ring buffer and
+ *             write out the transposed data to a DADA ring
+ *             buffer
+ */
+class TransposeClient
+{
+public:
+
+    TransposeClient(DadaWriteClient* _writer[],std::string* keys, std::uint32_t numbeams);
+    TransposeClient(TransposeClient const&) = delete;
+    ~TransposeClient();
+
     /**
-     * @brief      Class that provides means for doing transpose
-     *             by reading data from a DADA ring buffer and 
-     *             write out the transposed data to a DADA ring
-     *             buffer
-     */
-    class TransposeClient 
-    {
-    public:
-
-        TransposeClient(DadaWriteClient* _writer[],std::string* keys, std::uint32_t numbeams);
-        TransposeClient(TransposeClient const&) = delete;
-	~TransposeClient();
-
-        /**
 	 * @brief function to the do the transpose
 	 */
-        void do_transpose(RawBytes& _current_block,RawBytes& transposed_data, std::uint32_t beamnum); 
-        
+    void do_transpose(RawBytes& _current_block,RawBytes& transposed_data, std::uint32_t beamnum);
+
 	/**
 	 * @brief Function to read in the data to be transposed
 	 */
-        RawBytes& read_to_transpose(DadaReadClient& _reader);
+    RawBytes& read_to_transpose(DadaReadClient& _reader);
 
-        /**
+    /**
 	 * @brief Function to write transposed data to the dada buffer
 	 */
-        void write_transpose(RawBytes& transposed_data, DadaWriteClient& writer);
+    void write_transpose(RawBytes& transposed_data, DadaWriteClient& writer);
 
 
-       /**
-        * @brief Setter for the numbeams
-        */
+    /**
+     * @brief Setter for the numbeams
+     */
+    void set_nbeams(const int nbeams);
 
-        void set_nbeams(const int nbeams);   
-     
-       /**
-        * @brief Setter for frequency channels
-        */
+    /**
+     * @brief Setter for frequency channels
+     */
+    void set_nchans(const int nchans);
 
-        void set_nchans(const int nchans);
+    /**
+     * @brief Setter of number of samples
+     */
+    void set_nsamples(const int nsamples);
 
-       /**
-        * @brief Setter of number of samples
-        */
+    /**
+     * @brief Setter for number of time samples
+     */
+    void set_ntime(const int ntime);
 
-        void set_nsamples(const int nsamples);
-  
-       /**
-        * @brief Setter for number of time samples
-        */
+    /**
+     * @brief Setter for number of frequency blocks
+     */
+    void set_nfreq(const int _nfreq);
 
-        void set_ntime(const int ntime);
+    /**
+     * @ getter for number of beams
+     */
+    std::uint32_t nbeams();
 
-       /**
-        * @brief Setter for number of frequency blocks
-        */
+    /**
+     * @ getter for number of channels
+     */
+    std::uint32_t nchans();
 
-        void set_nfreq(const int _nfreq);
+    /**
+     * @ getter for number of time blocks
+     */
+    std::uint32_t nsamples();
 
-       /**
-        * @ getter for number of beams
-        */
+    /**
+     * @ getter for number of time samples
+     */
+    std::uint32_t ntime();
 
-        std::uint32_t nbeams();
- 
-       /**
-        * @ getter for number of channels
-        */
+    /**
+     * @ getter for number of freq blocks
+     */
+    std::uint32_t nfreq();
 
-        std::uint32_t nchans();
 
-       /**
-        * @ getter for number of time blocks
-        */
+private:
+    std::uint32_t _nbeams;
+    std::uint32_t _nchans;
+    std::uint32_t _nsamples;
+    std::uint32_t _ntime;
+    std::uint32_t _nfreq;
 
-        std::uint32_t nsamples();
+};
 
-       /**
-        * @ getter for number of time samples
-        */
-
-        std::uint32_t ntime();
-
-       /**
-        * @ getter for number of freq blocks
-        */
-
-        std::uint32_t nfreq();
- 
-
-    private:
-        std::uint32_t _nbeams;
-	std::uint32_t _nchans;
-	std::uint32_t _nsamples;
-	std::uint32_t _ntime;
-	std::uint32_t _nfreq;
-
-    };
 } //namespace psrdada_cpp
 
 #endif //PSRDADA_CPP_TRANSPOSE_CLIENT_HPP
