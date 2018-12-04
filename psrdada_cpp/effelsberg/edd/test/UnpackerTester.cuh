c325ce0e906ff9b35af35f93545e4f25ed1a8294
Ewan Barr <ewan.d.barr@googlemail.com>
2018-12-04 13:28:54 +0100

    added shells for Unpacker

---
+++ psrdada_cpp/effelsberg/edd/test/UnpackerTester.cuh
@@ -0,0 +1,45 @@
+#ifndef PSRDADA_CPP_EFFELSBERG_EDD_UNPACKERTESTER_CUH
+#define PSRDADA_CPP_EFFELSBERG_EDD_UNPACKERTESTER_CUH
+
+namespace psrdada_cpp {
+namespace effelsberg {
+namespace edd {
+namespace test {
+
+class UnpackerTester: public ::testing::Test
+{
+public:
+    typedef std::vector<uint64_t> InputType;
+    typedef std::vector<float> OutputType;
+
+protected:
+    void SetUp() override;
+    void TearDown() override;
+
+public:
+    UnpackerTester();
+    ~UnpackerTester();
+
+protected:
+    void unpacker_12_to_32_c_reference(
+        InputType const& input,
+        OutputType& output);
+
+    void unpacker_8_to_32_c_reference(
+        InputType const& input,
+        OutputType& output);
+
+    void compare_against_host(
+        Unpacker::OutputType const& gpu_output,
+        OutputType const& host_output);
+
+protected:
+    cudaStream_t _stream;
+};
+
+} //namespace test
+} //namespace edd
+} //namespace meerkat
+} //namespace psrdada_cpp
+
+#endif //PSRDADA_CPP_EFFELSBERG_EDD_UNPACKERTESTER_CUH
---
+++ psrdada_cpp/effelsberg/edd/test/src/UnpackerTester.cu
@@ -0,0 +1,74 @@
+#include "psrdada_cpp/effelsberg/edd/test/UnpackerTester.cuh"
+
+#define BSWAP64(x) ((0xFF00000000000000 & x) >> 56) | \
+                   ((0x00FF000000000000 & x) >> 40) | \
+                   ((0x0000FF0000000000 & x) >> 24) | \
+                   ((0x000000FF00000000 & x) >>  8) | \
+                   ((0x00000000FF000000 & x) <<  8) | \
+                   ((0x0000000000FF0000 & x) << 24) | \
+                   ((0x000000000000FF00 & x) << 40) | \
+                   ((0x00000000000000FF & x) << 56)
+
+namespace psrdada_cpp {
+namespace effelsberg {
+namespace edd {
+namespace test {
+
+UnpackerTester::UnpackerTester()
+    : ::testing::Test()
+    , _stream(0)
+{
+
+}
+
+UnpackerTester::~UnpackerTester()
+{
+
+}
+
+void UnpackerTester::SetUp()
+{
+    CUDA_ERROR_CHECK(cudaStreamCreate(&_stream));
+}
+
+void UnpackerTester::TearDown()
+{
+    CUDA_ERROR_CHECK(cudaStreamDestroy(_stream));
+}
+
+void UnpackerTester::unpacker_12_to_32_c_reference(
+    InputType const& input,
+    OutputType& output)
+{
+
+}
+
+void UnpackerTester::unpacker_8_to_32_c_reference(
+    InputType const& input,
+    OutputType& output)
+{
+
+}
+
+void UnpackerTester::compare_against_host(
+    Unpacker::OutputType const& gpu_output,
+    OutputType const& host_output)
+{
+
+}
+
+TEST_F(UnpackerTester, 12_bit_unpack_test)
+{
+
+}
+
+TEST_F(UnpackerTester, 8_bit_unpack_test)
+{
+
+}
+
+
+} //namespace test
+} //namespace edd
+} //namespace meerkat
+} //namespace psrdada_cpp
