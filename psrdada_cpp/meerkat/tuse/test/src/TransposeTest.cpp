7e5912014476c9e93925bc747454f129b285c506
kmrajwade <rkaustubh10@gmail.com>
2019-01-14 11:35:03 +0000

    Added src directory for unit testing

---
+++ psrdada_cpp/meerkat/tuse/test/src/TransposeTest.cpp
@@ -0,0 +1,35 @@
+#include "psrdada_cpp/meerkat/tuse/test/TransposeTest.h"
+#include "psrdada_cpp/meerkat/tuse/transpose_to_dada.h"
+
+namespace psrdada_cpp {
+namespace meerkat {
+namespace tuse {
+namespace test {
+
+TransposeTest::TransposeTest()
+    : ::testing::Test()
+{
+}
+
+TransposeTest::~TransposeTest()
+{
+}
+
+void TransposeTest::SetUp()
+{
+}
+
+void TransposeTest::TearDown()
+{
+}
+
+TEST_F(TransposeTest, test_attributes)
+{
+
+}
+
+} //namespace test
+} //namespace tuse
+} //namespace meerkat
+} //namespace psrdada_cpp
+
