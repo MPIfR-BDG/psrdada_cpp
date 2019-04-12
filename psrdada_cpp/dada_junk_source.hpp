9314f60fd16a30c0ce3ffb8c50e9cd43f100929f
root <root@fbfpn02.mpifr-be.mkat.karoo.kat.ac.za>
2019-04-12 11:28:43 +0000

    fixed junk source bug

--- psrdada_cpp/dada_junk_source.hpp
+++ psrdada_cpp/dada_junk_source.hpp
@@ -4,7 +4,7 @@
 #include "psrdada_cpp/raw_bytes.hpp"
 #include "psrdada_cpp/common.hpp"
 #include <vector>
-#include <ifstream>
+#include <fstream>
 
 namespace psrdada_cpp
 {
@@ -18,12 +18,12 @@ namespace psrdada_cpp
         std::vector<char> junk_header(header_size);
         std::vector<char> junk_block(nbytes_per_write);
         RawBytes header(junk_header.data(), header_size, header_size, false);
-        if (header_fname)
+        if (header_fname != "")
         {
             std::ifstream headerfile (header_fname);
             if (!headerfile.is_open())
             {
-                throw std::runtime_error("Unable to open header file")
+                throw std::runtime_error("Unable to open header file");
             }
             headerfile.read(header.ptr(), header.total_bytes());
             headerfile.close();
