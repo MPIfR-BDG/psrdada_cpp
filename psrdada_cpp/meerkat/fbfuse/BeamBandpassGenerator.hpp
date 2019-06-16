2179c3b5ae4617e9b8e91ed170208729ea78498b
Ewan Barr <ewan.d.barr@googlemail.com>
2019-06-16 22:54:53 +0200

    first pass on beam bandpass generator

---
+++ psrdada_cpp/meerkat/fbfuse/BeamBandpassGenerator.hpp
@@ -0,0 +1,59 @@
+#ifndef PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATOR_HPP
+#define PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATOR_HPP
+
+#include "psrdada_cpp/raw_bytes.hpp"
+#include "psrdada_cpp/common.hpp"
+#include <vector>
+
+namespace psrdada_cpp {
+namespace meerkat {
+namespace fbfuse {
+
+struct ChannelStatistics
+{
+    float mean;
+    float variance;
+};
+
+/**
+ * @brief      Class for coherent beamformer.
+ */
+template <typename Handler>
+class BeamBandpassGenerator
+{
+public:
+    /**
+     * @brief      Constructs a BeamBandpassGenerator object.
+     */
+    BeamBandpassGenerator(
+        unsigned int nbeams,
+        unsigned int nchans_per_subband,
+        unsigned int nsubbands,
+        unsigned int heap_size,
+        unsigned int nbuffer_acc,
+        Handler& handler);
+    ~BeamBandpassGenerator();
+    BeamBandpassGenerator(BeamBandpassGenerator const&) = delete;
+
+    void init(RawBytes&);
+    bool operator()(RawBytes&);
+
+private:
+    Handler& _handler;
+    unsigned int _nbeams,
+    unsigned int _nchans_per_subband,
+    unsigned int _nsubbands,
+    unsigned int _heap_size,
+    unsigned int _nbuffer_acc,
+    std::size_t _naccumulated;
+    std::size_t _count;
+    std::vector<ChannelStatistics> _output_buffer;
+};
+
+} //namespace fbfuse
+} //namespace meerkat
+} //namespace psrdada_cpp
+
+#include "psrdada_cpp/meerkat/fbfuse/detail/BeamBandpassGenerator.cpp"
+
+#endif //PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATOR_HPP
---
+++ psrdada_cpp/meerkat/fbfuse/test/BeamBandpassGeneratorTester.hpp
@@ -0,0 +1,29 @@
+#ifndef PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATORTESTER_CUH
+#define PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATORTESTER_CUH
+
+#include "psrdada_cpp/meerkat/fbfuse/BeamBandpassGenerator.hpp"
+#include <gtest/gtest.h>
+
+namespace psrdada_cpp {
+namespace meerkat {
+namespace fbfuse {
+namespace test {
+
+class BeamBandpassGeneratorTester: public ::testing::Test
+{
+protected:
+    void SetUp() override;
+    void TearDown() override;
+
+public:
+    BeamBandpassGeneratorTester();
+    ~BeamBandpassGeneratorTester();
+
+};
+
+} //namespace test
+} //namespace fbfuse
+} //namespace meerkat
+} //namespace psrdada_cpp
+
+#endif //PSRDADA_CPP_MEERKAT_FBFUSE_BEAMBANDPASSGENERATORTESTER_CUH
